df295fe5425cd3c42b237f5edfd4668ffe7ebab6
Ashmit <ashmit.singh.k@gmail.com>
2022-09-25 00:32:11 +0530

    cleaned code

--- generateQR.py
+++ generateQR.py
@@ -1,10 +1,7 @@
 # Import QRCode from pyqrcode
 import pyqrcode
 import png
-from pyqrcode import QRCode
 
 def generateQR(studentID):
     url = pyqrcode.create(studentID)
     url.png("./QR/QR_STUDENT_" + studentID + ".png", scale = 6)
-
-generateQR("1")
